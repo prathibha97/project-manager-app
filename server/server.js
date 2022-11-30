5eb3edd2b7a40c8d0ff836df2c7537fd3b98e907
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-30 23:31:01 +0530

    Initial app setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+  console.log(`server listening on port ${port}`)
+})
