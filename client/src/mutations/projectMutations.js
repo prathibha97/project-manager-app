95f8d9638807712f6258600ee7bc2659a8daa06a
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-01 14:45:46 +0530

    Add edit project form

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -16,11 +16,26 @@ mutation AddProject($name:String!, $description: String!, $status:ProjectStatus!
   }
 }
 `
-
 export const DELETE_PROJECT = gql`
 mutation DeleteProject($id: ID!){
   deleteProject(id: $id){
       id
+  }
+}
+`
+export const UPDATE_PROJECT = gql`
+mutation UpdateProject($id:ID!, $name:String!, $description: String!, $status:ProjectStatusUpdate!){
+  UpdateProject(id: $id, name: $name, description: $description, status: $status){
+    id
+    name
+    description
+    status
+    client{
+      id 
+      name
+      email
+      phone
+    }
   }
 }
 `
