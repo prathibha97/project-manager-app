761ee00d1da08ba63431c3437899f5cef827b7b4
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-01 15:16:55 +0530

    Deployment preparation

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
-const cors = require('cors');
+const cors = require('cors')
+const path = require('path')
 const connectDB = require('./config/db')
 const schema = require('./schema/schema')
 
 const app = express()
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development'
 }))
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000
 
